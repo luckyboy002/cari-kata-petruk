d7a6d16f5e3c417a8891d40499b2891fc709876f
DYmaz <152365689+DYmazeh@users.noreply.github.com>
2024-04-28 17:13:02 +0700

    Update CariKata.cpp
     Berkontribusi pada penulisan fungsi searchWord,
    - Memastikan fungsi berjalan dengan benar dan mengembalikan nilai true jika kata ditemukan, dan false jika tidak.
    - Membantu dalam merancang dan merencanakan logika pencarian kata dalam matriks.
    - Mengevaluasi dan memberikan masukan pada algoritma yang diusulkan oleh Fathur.
    - Memastikan bahwa logika pencarian kata berjalan dengan baik dan efisien.

--- CariKata.cpp
+++ CariKata.cpp
@@ -10,3 +10,29 @@ const int COL = 25;
 bool searchWord(char grid[ROW][COL], string word) {
     int len = word.length();
 
+  if (len > ROW && len > COL) {
+        return false;
+    }
+
+    for (int row = 0; row < ROW; row++) {
+        for (int col = 0; col < COL; col++) {
+            if (tolower(grid[row][col]) == tolower(word[0])) {
+                for (int x = -1; x <= 1; x++) {
+                    for (int y = -1; y <= 1; y++) {
+                        int k, rd = row + x, cd = col + y;
+                        for (k = 1; k < len; k++) {
+                            if (rd >= ROW || rd < 0 || cd >= COL || cd < 0)
+                                break;
+                            if (tolower(grid[rd][cd]) != tolower(word[k]))
+                                break;
+                            rd += x, cd += y;
+                        }
+                        if (k == len)
+                            return true;
+                    }
+                }
+            }
+        }
+    }
+    return false;
+}
